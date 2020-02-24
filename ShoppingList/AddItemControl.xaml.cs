e54a908933c2d5ace9895986675d996b029730b6
Regan Hale <rshale@users.campus>
2020-02-24 17:07:10 -0600

    Add project files.

---
+++ ShoppingList/AddItemControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList
+{
+    /// <summary>
+    /// Interaction logic for AddItemControl.xaml
+    /// </summary>
+    public partial class AddItemControl : UserControl
+    {
+        public AddItemControl()
+        {
+            InitializeComponent();
+            AddItemButton.Click += OnAddItemButtonClick;
+        }
+
+        protected void OnAddItemButtonClick(object sender, RoutedEventArgs e)
+        {
+            var item = ItemToAdd.Text;
+            /*
+            var data = (ShoppingListData)DataContext; //casting operator 1 (throws exception if cannot cast) wrap in try-catch block
+            var data2 = DataContext as ShoppingListData;  //casting operator 2 (resolves to null if cannot cast) if silent failure is desired
+            if(data2 != null)
+            {
+                //safe to work with data2
+            }
+
+            if(DataContext is ShoppingListData data3)
+            {
+                //evaluates to boolean, checks typeof; another way to check casting
+                //if evaluates to true, sets data3
+                //BUT data3 only exists inside this block
+            }
+            */
+            if(DataContext is ShoppingListData data)
+            {
+                data.Add(ItemToAdd.Text);
+                ItemToAdd.Text = "";
+            }
+
+
+        }
+    }
+}
---
+++ ShoppingList/ShoppingListData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ShoppingList
+{
+    public class ShoppingListData: INotifyPropertyChanged
+    {
+        List<string> items = new List<string>();
+
+        public IEnumerable<string> Items => items.ToArray();   //must be careful here if want to preserve true data hiding
+
+        public event PropertyChangedEventHandler PropertyChanged; //must read like this
+
+        //if using a reference type here, public property can have direct control of private backing variable
+
+        public void Add(string item)
+        {
+            items.Add(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items")); //Critically important this is spelled EXACTLY the same
+            //would be null if there is no "listener" or binding on object
+        }
+
+        public void Remove(string item)
+        {
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+    }
+}
